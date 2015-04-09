95876f17f96cf06542e050643c12f93731837ae1
Jonathan Kemp <kempdogg@gmail.com>
2015-04-09 15:31:10 -0400

    Remove logging and add globals for jshint.

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/* global describe, it */
+
 'use strict';
 
 var assert = require('assert'),
@@ -6,7 +8,6 @@ var assert = require('assert'),
 describe('Selector', function () {
     it('should return an object', function () {
         var selector = new Selector('body', [ 0, 0, 0, 1 ]);
-        console.log(selector);
         assert(selector);
         assert.equal(selector.text, 'body');
         assert.deepEqual(selector.spec, [ 0, 0, 0, 1 ]);
@@ -16,7 +17,6 @@ describe('Selector', function () {
 describe('Selector.parsed', function () {
     it('should get parsed selector', function () {
         var selector = new Selector('body');
-        console.log(selector.parsed());
         assert(selector.parsed());
         assert.equal(selector.parsed()['0'].tag, 'body');
         assert.equal(selector.parsed().length, 1);
@@ -26,7 +26,6 @@ describe('Selector.parsed', function () {
 describe('Selector.specificity', function () {
     it('should get specificity', function () {
         var selector = new Selector('body');
-        console.log(selector.specificity());
         assert.deepEqual(selector.specificity(), [ 0, 0, 0, 1 ]);
     });
 });
