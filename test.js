2711712f040c64198dba0a77c726174926016b59
Jonathan Kemp <kempdogg@gmail.com>
2016-03-21 20:12:13 +0000

    Update tests for v2.

--- test.js
+++ test.js
@@ -3,11 +3,11 @@
 'use strict';
 
 var assert = require('assert'),
-    Selector = require('./');
+    sel = require('./');
 
 describe('Selector', function () {
     it('should return an object', function () {
-        var selector = new Selector('body', [ 0, 0, 0, 1 ]);
+        var selector = sel('body', [ 0, 0, 0, 1 ]);
         assert(selector);
         assert.equal(selector.text, 'body');
         assert.deepEqual(selector.spec, [ 0, 0, 0, 1 ]);
@@ -16,7 +16,7 @@ describe('Selector', function () {
 
 describe('Selector.parsed', function () {
     it('should get parsed selector', function () {
-        var selector = new Selector('body');
+        var selector = sel('body');
         assert(selector.parsed());
         assert.equal(selector.parsed()['0'].tag, 'body');
         assert.equal(selector.parsed().length, 1);
@@ -25,7 +25,7 @@ describe('Selector.parsed', function () {
 
 describe('Selector.specificity', function () {
     it('should get specificity', function () {
-        var selector = new Selector('body');
+        var selector = sel('body');
         assert.deepEqual(selector.specificity(), [ 0, 0, 0, 1 ]);
     });
 });
