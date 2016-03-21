1365f0e9c287f0049ec8825f2f9e8abc6b9379bf
Jonathan Kemp <kempdogg@gmail.com>
2016-03-21 20:11:50 +0000

    Eliminate use of new.

--- index.js
+++ index.js
@@ -22,6 +22,71 @@ function parse(text) {
 }
 
 /**
+* Returns specificity based on selector text and tokens.
+*
+* @param {String} selector
+* @param {Array} tokens
+* @api private.
+*/
+
+function getSpecificity(text, parsed) {
+    var expressions = parsed || parse(text),
+        spec = [ 0, 0, 0, 0 ],
+        nots = [],
+        i,
+        expression,
+        pseudos,
+        p,
+        ii,
+        not,
+        jj;
+
+    for (i = 0; i < expressions.length; i++) {
+        expression = expressions[i];
+        pseudos = expression.pseudos;
+
+        // id awards a point in the second column
+        if (expression.id) {
+            spec[1]++;
+        }
+
+        // classes and attributes award a point each in the third column
+        if (expression.attributes) {
+            spec[2] += expression.attributes.length;
+        }
+        if (expression.classList) {
+            spec[2] += expression.classList.length;
+        }
+
+        // tag awards a point in the fourth column
+        if (expression.tag && expression.tag !== '*') {
+            spec[3]++;
+        }
+
+        // pseudos award a point each in the fourth column
+        if (pseudos) {
+            spec[3] += pseudos.length;
+
+            for (p = 0; p < pseudos.length; p++) {
+                if (pseudos[p].key === 'not') {
+                    nots.push(pseudos[p].value);
+                    spec[3]--;
+                }
+            }
+        }
+    }
+
+    for (ii = nots.length; ii--;) {
+        not = getSpecificity(nots[ii]);
+        for (jj = 4; jj--;) {
+            spec[jj] += not[jj];
+        }
+    }
+
+    return spec;
+}
+
+/**
  * CSS selector constructor.
  *
  * @param {String} selector text
@@ -29,97 +94,43 @@ function parse(text) {
  * @api public
  */
 
-function Selector(text, spec) {
-    this.text = text;
-    this.spec = spec;
-}
+module.exports = function (text, spec) {
+    var tokens,
+        _spec,
 
-/**
- * Get parsed selector.
- *
- * @api public
- */
+        /**
+         * Get parsed selector.
+         *
+         * @api public
+         */
 
-Selector.prototype.parsed = function () {
-    if (!this.tokens) {
-        this.tokens = parse(this.text);
-    }
-    return this.tokens;
+        parsed = function () {
+            if (!tokens) {
+                tokens = parse(text);
+            }
+            return tokens;
+        },
+
+        /**
+         * Lazy specificity getter
+         *
+         * @api public
+         */
+
+        specificity = function () {
+            if (!spec) {
+                _spec = getSpecificity(text, parsed());
+            }
+            return _spec;
+        };
+
+    return {
+        text: text,
+
+        spec: spec,
+
+        parsed: parsed,
+
+        specificity: specificity
+    };
 };
-
-/**
- * Lazy specificity getter
- *
- * @api public
- */
-
-Selector.prototype.specificity = function () {
-
-    function specificity(text, parsed) {
-        var expressions = parsed || parse(text),
-            spec = [ 0, 0, 0, 0 ],
-            nots = [],
-            i,
-            expression,
-            pseudos,
-            p,
-            ii,
-            not,
-            jj;
-
-        for (i = 0; i < expressions.length; i++) {
-            expression = expressions[i];
-            pseudos = expression.pseudos;
-
-            // id awards a point in the second column
-            if (expression.id) {
-                spec[1]++;
-            }
-
-            // classes and attributes award a point each in the third column
-            if (expression.attributes) {
-                spec[2] += expression.attributes.length;
-            }
-            if (expression.classList) {
-                spec[2] += expression.classList.length;
-            }
-
-            // tag awards a point in the fourth column
-            if (expression.tag && expression.tag !== '*') {
-                spec[3]++;
-            }
-
-            // pseudos award a point each in the fourth column
-            if (pseudos) {
-                spec[3] += pseudos.length;
-
-                for (p = 0; p < pseudos.length; p++) {
-                    if (pseudos[p].key === 'not') {
-                        nots.push(pseudos[p].value);
-                        spec[3]--;
-                    }
-                }
-            }
-        }
-
-        for (ii = nots.length; ii--;) {
-            not = specificity(nots[ii]);
-            for (jj = 4; jj--;) {
-                spec[jj] += not[jj];
-            }
-        }
-
-        return spec;
-    }
-
-    if (!this.spec) {
-        this.spec = specificity(this.text, this.parsed());
-    }
-    return this.spec;
-};
-
-/**
-* Module exports.
-*/
-
-module.exports = exports = Selector;
